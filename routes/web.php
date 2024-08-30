7873acff3fc6bb51f79f73666376908560f03041
miyake <myk19977@gmail.com>
2024-08-31 01:09:46 +0900

    add: articles ルーティング

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('hello');
 });
+
+
+Route::get('/articles', [ArticleController::class, 'index'])->name('articles.index');
