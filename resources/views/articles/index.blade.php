6dc8068db19cd965cba29c3aab00d1c8d9abcbca
miyake <myk19977@gmail.com>
2024-08-31 01:01:42 +0900

    add: articlesビュー

---
+++ resources/views/articles/index.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1 class="site-title">ブログ</h1>
+    </header>
+    <main class="container">
+        @foreach ($articles as $article)
+        <article class="article-item">
+            <div class="article-title">{{ $article->title }}</div>
+            <div class="article-body">{{ $article->body }}</div>
+        </article>
+        @endforeach
+    </main>
+    <footer>
+        テキスト
+    </footer>
+</body>
+</html>
